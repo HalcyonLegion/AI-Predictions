69698e398811007bfdde989baa9d56c65a4f6d78
Lee Webb <leewerbb55@hotmail.com>
2023-10-31 15:19:01 +0800

    1st edition for testing

--- app.py
+++ app.py
@@ -7,22 +7,38 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/get-fixtures', methods=['POST'])
+@app.route("/get-fixtures", methods=["POST"])
 def get_fixtures():
-    fixture_date = request.form['date']
-    
-    url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
-    querystring = {"date": fixture_date, "league":"39", "season":"2023"}
+    fixture_date = request.form.get("date")
+
+    fixture_url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
     headers = {
         "X-RapidAPI-Key": "ef97436974msha2f90f1e83d332fp1e6007jsnc60c6c3cc314",
         "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
     }
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
+    querystring = {"date": fixture_date, "league": "39", "season": "2023"}
+    fixture_response = requests.get(fixture_url, headers=headers, params=querystring)
+    fixture_data = fixture_response.json()
 
-    fixtures = [(match['fixture']['id'], match['teams']['home']['name'], match['teams']['away']['name']) for match in data['response']]
+    fixture_ids = [(match["fixture"]["id"], match["teams"]["home"]["name"], match["teams"]["away"]["name"]) for match in fixture_data["response"]]
+
+    results = []
+    for fixture in fixture_ids:
+        fixture_id = fixture[0]
+        home_team = fixture[1]
+        away_team = fixture[2]
+
+        prediction_url = "https://api-football-v1.p.rapidapi.com/v3/predictions"
+        prediction_querystring = {"fixture": fixture_id}
 	
-    return render_template('index.html', fixtures=fixtures)
+        prediction_response = requests.get(prediction_url, headers=headers, params=prediction_querystring)
+        prediction_data = prediction_response.json()
+
+        if prediction_data.get("response"):  # Check to make sure there is a result
+            winner = prediction_data["response"][0]["predictions"]["winner"]["name"]
+            results.append((fixture_id, home_team, away_team, winner))
+
+    return render_template("index.html", results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
